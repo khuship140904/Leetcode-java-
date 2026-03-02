016e950b76b4b0b993bdc2a1867b9cc810a754cd
Khushi Patel <khushipatel140904@gmail.com>
2026-03-02 21:12:51 +0530

    2 feb

--- J.java
+++ J.java
@@ -1,5 +1,27 @@
-//remove duplicates
+//27. Remove Element
 
 public class J {
+    public int removeElement(int[] nums, int val) {
+        int count =0;
+        for(int i=0;i<nums.length;i++){
+            if (nums[i]!=val) {
+                nums[count]=nums[i]; 
+                count++;
+            }
+        }
+        return count;  
+    }
+
+    public static void main(String[] args) {
+        int val = 3;
+        int[] nums = {3,2,2,3};
+
+        J j= new J();
+
+        int count = j.removeElement(nums, val);
+
+        System.out.println(count);
+         
+    }
     
 }
