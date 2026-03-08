c88996dbea8f377ca5ad69dd00aa1049c17010f6
Khushi Patel <khushipatel140904@gmail.com>
2026-03-08 08:47:15 +0530

    8 march

--- K.java
+++ K.java
@@ -1,8 +1,22 @@
-//sorted array
+//344. Reverse String
 
 public class K {
+    public void reverseString(char[] s) {
+        int j=s.length-1;
+        for(int i=0;i<s.length;i++){
+            s[i]=s[j--];
+            }   
+    }
     public static void main(String[] args) {
-        
+        K k = new K();
+        char[] s = {'h','e','l','l','o'};
+
+
+        k.reverseString(s);
+
+        for(int i=0;i<s.length;i++){
+            System.out.println(s[i]);
+        }
     }
     
 }
