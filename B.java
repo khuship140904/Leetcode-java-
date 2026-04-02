306a8199f208236d43dbdbbc2ca51fe8f3b63cc2
Khushi Patel <khushipatel140904@gmail.com>
2026-04-02 21:09:07 +0530

    2 April

--- B.java
+++ B.java
@@ -1,32 +1,40 @@
 //1796. Second Largest Digit in a String
 
 public class B {
-     public int secondHighest(String s) {
-        int high = -1;
-        int secondhigh = -1;
-
+    public int secondHighest(String s) {
+        int highest = -1;
+        int secondhighest = -1;
+        
         for(int i=0;i<s.length();i++){
-         char c = s.charAt(i);
+        
+         //ek ek character ko visit kara string ke 
+         Character c = s.charAt(i);
 
-         if (Character.isDigit(c)) {
+         //check karo ki character digit hai yaa nahi 
+            if(Character.isDigit(c)) {
 
-            int num = c -'0';
+              //agar true hua toh number variable me assign karo 
+              //yaha - '0' isliye kara hai kyoki character me number ki value bhi ascii value pe 
+              // store hoti hai toh usko integer me convert karne ke liye esa kara 
+              int num = c -'0';
 
-            if (num>high) {
-                secondhigh=high;
-                high=num;
-            }else if (num>secondhigh && num!=high){
-                secondhigh=num;  
-            }
-         }
+                if(num>highest) {
+                secondhighest=highest;
+                highest=num;
+
+                }else if (num>secondhighest && num!=highest){
+                secondhighest=num;  
+                }
+           }
         }
-        return secondhigh;
+        
+        return secondhighest;
     }
 
      public static void main(String[] args) {
         B obj = new B();
 
-        String s = "abc1111";
+        String s = "abc5161";
 
         int secondhigh = obj.secondHighest(s);
 
