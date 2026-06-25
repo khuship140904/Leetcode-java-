afecfe5f90e5c31ebf6a7b9de4278a71e87c5cb0
Khushi Patel <khushipatel140904@gmail.com>
2026-06-25 16:40:05 +0530

    25 june

--- Hack10.java
+++ Hack10.java
@@ -1,3 +1,45 @@
+//Java Anagrams
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Hack10 {
+     static boolean isAnagram(String a, String b) {
+        
+        String a1 = a.toLowerCase();
+        String b1 = b.toLowerCase();
+
+        boolean y = false;
+
+        if(a1.length()==b1.length()){
+            char[] a2 = a1.toCharArray();
+            char[] b2 = b1.toCharArray();
+
+            Arrays.sort(a2);
+            Arrays.sort(b2);
+
+            String a3 = new String(a2);
+            String b3 = new String(b2);
+
+            if(a3.equals(b3)){
+                y=true;
+            }
+
+        }else{
+            y=false;
+        }
+
+        return y;
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
     
 }
