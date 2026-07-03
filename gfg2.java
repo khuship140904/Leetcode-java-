3b7e4714e3500f1540bd1d4a63736acfde611814
Khushi Patel <khushipatel140904@gmail.com>
2026-07-03 11:29:05 +0530

    3 july

---
+++ gfg2.java
@@ -0,0 +1,10 @@
+//convert string to lower case
+public class gfg2 {
+    static String toLower(String s) {
+        
+        String lower = s.toLowerCase();
+        
+        return lower;
+        
+    }
+}
