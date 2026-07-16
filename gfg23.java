27d86a73cf4dfad0b61a5ed39b0927879c6720d0
Khushi Patel <khushipatel140904@gmail.com>
2026-07-16 07:57:01 +0530

    16 july

--- gfg23.java
+++ gfg23.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
+//input boolean value
 public class gfg23 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean a;
+
+        // code here
+        boolean a = sc.hasNextBoolean();
+
+        // Printing numeric representation
+        System.out.print(a ? 1 : 0);
+    }
 }
---
+++ gfg24.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+//Print Statement
+public class gfg24 {
+     void utility(Scanner scn) {
+        // code here
+        String s = scn.nextLine();
+        
+        System.out.println(s);
+    }
+}
