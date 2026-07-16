a5665a47335d580a408c2a48a71374be3a003807
Khushi Patel <khushipatel140904@gmail.com>
2026-07-16 08:06:41 +0530

    16 july

---
+++ gfg25.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//the if statement
+public class gfg25 {
+   public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // code here
+        if(n>100){
+            System.out.println("Big");
+        }
+        
+           System.out.println("Number"); 
+        
+    }
+}
