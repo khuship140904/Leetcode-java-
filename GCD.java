c68e6168b61eaed7e5ca0b7e8b77a02926f3716a
Khushi Patel <khushipatel140904@gmail.com>
2026-08-30 14:33:01 +0530

    30 August

--- GCD.java
+++ GCD.java
@@ -1,3 +1,19 @@
 public class GCD {
-    
+    public int GCD(int n1, int n2) {
+        int gcd = 1;
+
+        for(int i=1,j=0;i<n1;i++,j++){
+            if(n1%i==0 && n2%i==0){
+                gcd = i;
+            }
+        }
+
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+        GCD gcd = new GCD();
+        System.out.println(gcd.GCD(6,12));
+        
+    }
 }
